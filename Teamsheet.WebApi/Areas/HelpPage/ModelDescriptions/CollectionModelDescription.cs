ed3f42698afff53310f05a5ab3164c2880d2dfe0
mh453 <hussainmajid95@gmail.com>
2016-11-27 15:32:35 +0000

    Config teamsheet for web api. Removed MVC Project folder.

---
+++ Teamsheet.WebApi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Teamsheet.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Teamsheet.WebApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Teamsheet.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
