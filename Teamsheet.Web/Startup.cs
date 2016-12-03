1a7e1c84b37b6fa9401f2ce57249f2366f2f252a
isambard <wheelerisambard@hotmail.co.uk>
2016-12-03 12:04:47 +0000

    Added a web file

---
+++ Teamsheet.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Teamsheet.Web.Startup))]
+namespace Teamsheet.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
