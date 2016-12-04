2e034d6d816356a50707c7a3139bf7da44bbf419
mh453 <hussainmajid95@gmail.com>
2016-12-04 11:21:22 +0000

    Add Seed Method To Create Manager And Employee Roles And Some Test Users

--- Teamsheet.Data/Migrations/Configuration.cs
+++ Teamsheet.Data/Migrations/Configuration.cs
@@ -1,5 +1,8 @@
 namespace Teamsheet.Data.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -18,14 +21,67 @@ namespace Teamsheet.Data.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+
+            var roleStore = new RoleStore<IdentityRole>(context);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
+
+            // Add seed users
+            if (!context.Users.Any(u => u.UserName == "test@someprovider.com"))
+            {
+
+                var user = new ApplicationUser
+                {
+                    Name = "TestUser",
+                    UserName = "test@someprovider.com",
+                    Email = "test@someprovider.com"
+                };
+
+                userManager.Create(user, "Password123!");
+            }
+
+            // Add seed users with roles
+            if (!context.Roles.Any(r => r.Name == "Manager"))
+            {
+                //Create a manager role
+                roleManager.Create(new IdentityRole { Name = "Manager" });
+            }
+
+            if(!context.Roles.Any(r => r.Name == "Employee"))
+            { 
+                //Create a employee role
+                roleManager.Create(new IdentityRole { Name = "Employee" });
+            }
+
+            //Create a user with roles of manager
+            if(!context.Users.Any(u => u.UserName == "test2@someprovider.com"))
+            {
+                var user = new ApplicationUser
+                {
+                    Name = "TestUser2",
+                    UserName = "test2@someprovider.com",
+                    Email = "test2@someprovider.com"
+                };
+
+                userManager.Create(user, "Password123!");
+
+                //Assign TestUser2 to Manager role
+                userManager.AddToRole(user.Id, "Manager");
+            }
+
+            if(!context.Users.Any(u => u.UserName == "test3@someprovide.com"))
+            {
+                var user = new ApplicationUser
+                {
+                    Name = "TestUser3",
+                    UserName = "test3@someprovide.com",
+                    Email = "test3@someprovide.com"
+                };
+
+                userManager.Create(user, "Password123!");
+                userManager.AddToRole(user.Id, "Employee");    
+            }
         }
     }
 }
